1ad17f0a5006a830e554e2bfc623ee082dbfc3de
James Doc <james@globe.church>
2022-12-22 08:01:45 +0000

    Expose author profile photo in rest API

--- src/mods/globe_userProfile.php
+++ src/mods/globe_userProfile.php
@@ -7,7 +7,6 @@
     include( $view );
   }
 
-
   function save_extra_user_profile_fields( $user_id ) {
     if ( empty( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'update-user_' . $user_id ) ) {
         return;
@@ -20,12 +19,28 @@
     update_user_meta( $user_id, 'glb_userAvatar', $_POST['glb_userAvatar'] );
   }
 
+  function glb_rest_get_user_profile_picture( $object, $field_name, $request  ) {
+    $user_id = $object['id'];
+    $media_id = get_user_meta($user_id, 'glb_userAvatar', true);
+    if (!$media_id) { return; }
+    return wp_get_attachment_url($media_id);
+  }
 
-  // TODO: Expose glb_userAvatar in REST API
-  // /wp-json/wp/v2/users/
+  function glb_rest_add_user_profile(){
+    register_rest_field( array('user'),
+      'profile_photo_url',
+      array(
+        'get_callback'    => 'glb_rest_get_user_profile_picture',
+        'update_callback' => null,
+        'schema'          => null,
+      )
+    );
+  }
 
   add_action( 'show_user_profile', 'extra_user_profile_fields' );
   add_action( 'edit_user_profile', 'extra_user_profile_fields' );
 
   add_action( 'personal_options_update', 'save_extra_user_profile_fields' );
   add_action( 'edit_user_profile_update', 'save_extra_user_profile_fields' );
+
+  add_action('rest_api_init', 'glb_rest_add_user_profile' );
