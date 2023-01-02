fd5c441eac1ba78e1f4b8989d679224dc1403f5c
James Doc <james@globe.church>
2023-01-02 18:01:06 +0000

    Fix: Series artwork can be replaced

--- src/views/sermon_series_edit_fields.php
+++ src/views/sermon_series_edit_fields.php
@@ -24,10 +24,9 @@
   </th>
   <td>
     <?php if( isset($image_id) && $image = wp_get_attachment_image_url( $image_id, 'medium' ) ) : ?>
-      <a href="#" class="glb-upload">
-        <img src="<?php echo esc_url( $image ) ?>" alt="" />
-      </a><br />
-      <a href="#" class="glb-remove">Remove image</a>
+      <img src="<?php echo esc_url( $image ) ?>" alt="" /><br />
+      <a href="#" class="button button-primary glb-upload">Replace image</a>
+      <a href="#" class="button glb-remove">Remove image</a>
       <input type="hidden" name="glb_sermon_artwork" value="<?php echo absint( $image_id ) ?>">
     <?php else : ?>
       <a href="#" class="button glb-upload">Upload image</a>
